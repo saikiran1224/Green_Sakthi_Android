a4786f69d5e10f48cbf2d7f4d0a248a500ed3de3
saikiran1224 <knvrssaikiran@gmail.com>
2022-05-30 00:59:34 +0530

    Designed Home Page and added Cart functionality for Zeca Diesel product.

--- app/src/main/java/com/greenshakthi/android/home/HomeFragment.kt
+++ app/src/main/java/com/greenshakthi/android/home/HomeFragment.kt
@@ -1,19 +1,134 @@
 package com.greenshakthi.android.home
 
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.icu.lang.UProperty.INT_START
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.google.android.material.card.MaterialCardView
 import com.greenshakthi.android.R
+import com.greenshakthi.android.utils.AppPreferences
+import org.w3c.dom.Text
+
 
 class HomeFragment : Fragment() {
 
+    lateinit var txtCustomerName: TextView
+    lateinit var txtDieselPrice: TextView
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
+        AppPreferences.init(requireContext())
+
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        txtCustomerName = view.findViewById(R.id.txtCustomerName)
+        txtDieselPrice = view.findViewById(R.id.txtDieselPrice)
+
+        // TODO: Need to set the Diesel price automatically from DB
+        // TODO: Check the Decimal Multiplication Problem and comma(,) representation
+
+        val customerName = AppPreferences.customerName.toString()
+        val str = SpannableStringBuilder("Hello $customerName,\nFuel up your vehicle Now!")
+        str.setSpan(StyleSpan(Typeface.BOLD), 6, 6 + customerName.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        txtCustomerName.text = str
+
+
+        // Add to Cart Layout
+        // normal AddToCart Layout
+
+        var tempQuantity = 0
+        var finalPrice = 0
+
+        val finalPriceMaterialCardView: MaterialCardView = view.findViewById(R.id.finalPriceCard)
+        val txtFinalPrice: TextView = view.findViewById(R.id.txtFinalPrice)
+        val txtPayButton: TextView = view.findViewById(R.id.txtPayButton)
+
+        val addToCartLayout: LinearLayout = view.findViewById(R.id.addToCartLayout)
+        val itemQuantity: TextView = view.findViewById(R.id.item_quantity)
+        val itemPlus: TextView = view.findViewById(R.id.item_plus)
+        val itemMinus: TextView = view.findViewById(R.id.item_minus)
+
+        // When User clicks Add to Cart Button
+        addToCartLayout.setOnClickListener {
+
+            if (itemQuantity.text.equals("Add to Cart")) {
+
+                // changing the text color of +, - and Quantity
+                itemPlus.setTextColor(requireContext().resources.getColor(R.color.white))
+                itemMinus.setTextColor(requireContext().resources.getColor(R.color.white))
+                itemQuantity.setTextColor(requireContext().resources.getColor(R.color.white))
+
+                itemPlus.visibility = View.VISIBLE
+                itemMinus.visibility = View.VISIBLE
+
+                // since its the first time we are setting the quantity to 1
+                itemQuantity.text = 10.toString()
+
+                // showing the Final Price card along with the price
+                finalPriceMaterialCardView.visibility = View.VISIBLE
+                txtFinalPrice.text = ((itemQuantity.text.toString().toInt() *txtDieselPrice.text.toString().toInt()).toString())
+
+            }
+        }
+
+        itemPlus.setOnClickListener {
+
+            // retrieving the current itemQuantity
+            tempQuantity = (itemQuantity.text.toString()).toInt()
+
+            // incrementing the quantity
+            tempQuantity++
+
+            // setting on the itemQuantity
+            itemQuantity.text = tempQuantity.toString()
+
+            // updating the text on Final Price Card
+            txtFinalPrice.text = ((itemQuantity.text.toString().toInt() *txtDieselPrice.text.toString().toInt()).toString())
+
+        }
+
+        itemMinus.setOnClickListener {
+
+            // retrieving the current itemQuantity
+            tempQuantity = (itemQuantity.text.toString()).toInt()
+
+            // decrementing the quantity
+            tempQuantity--
+
+            // if quantity reached 0 then disable colored layout and get back to normal layout
+            if (tempQuantity < 10 ) {
+
+                // disappearing the Final Price Material Card Viww
+                finalPriceMaterialCardView.visibility = View.GONE
+
+                itemPlus.visibility = View.GONE
+                itemMinus.visibility = View.GONE
+                itemQuantity.setTextColor(requireContext().resources.getColor(R.color.white))
+                itemQuantity.text = "Add to Cart"
+
+            }
+            // if quantity > 0 remain the same layout and perform the basic operation as in add operation
+            else {
+
+                // setting on the itemQuantity
+                itemQuantity.text = tempQuantity.toString()
+
+                // updating the price on Final Price Card
+                txtFinalPrice.text = ((itemQuantity.text.toString().toInt() *txtDieselPrice.text.toString().toInt()).toString())
+
+            }
+        }
 
         return view
     }
